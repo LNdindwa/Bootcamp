2d5ad19743cd97bbd84c30465895140412735d10
LNdindwa <lndindwa@gmail.com>
2023-04-04 17:04:18 +0200

    more changes

--- JavaScripts_Basics/jsbasics.js
+++ JavaScripts_Basics/jsbasics.js
@@ -28,6 +28,38 @@ let arr1 = [1,2,3,5,10,100,4,6,12];
 // arr1.push(1001);
 // console.log(arr1);
 
-arr1.push("lihle");
-arr1.unshift("Ndindwa")
-console.log(arr1);
+// arr1.push("lihle");
+// arr1.unshift("Ndindwa")
+// console.log(arr1);
+
+// let goat = arr1.splice(4,2);
+
+//Ascending order
+// let res = arr1.sort(
+// //     (a,b)=>{
+// //     return a - b;
+// // }
+// )
+
+//Descending order
+// let res = arr1.sort(
+//     (a,b)=>{
+//         return b - a;
+//     }
+// )
+// console.log(goat);
+
+//Literal object
+let emp = {};
+
+let person = {
+    fname: "Lihle",
+    lname: "Ndindwa",
+    speak: ()=>{
+        console.log(person.lname);
+    }
+}
+person.mname = "dwight";
+
+console.log(person.mname);
+console.log(person);
