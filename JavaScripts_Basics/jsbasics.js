745bf6f581557d757a2a9ae542812ab5b004d4f0
LNdindwa <lndindwa@gmail.com>
2023-04-04 14:43:39 +0200

    second commit

---
+++ JavaScripts_Basics/jsbasics.js
@@ -0,0 +1,33 @@
+// function add(a,b){
+//     return a+b;
+    
+// }
+
+// console.log(add(5,15));
+
+// let a = 20;
+// let b = "20"
+// if(a>b){
+//     console.log("yes a is = ")
+// }
+
+// let nums = [1,2,3,8,10]
+// let res = nums [3]
+// console.log(res)
+
+//check for numbers divisible by three
+let arr1 = [1,2,3,5,10,100,4,6,12];
+
+
+// for (let k = 5; k < arr1.length ; k++) {
+//     if(arr1[k]% 3 === 0){
+//         console.log(arr1[k]);
+//     }
+// }
+
+// arr1.push(1001);
+// console.log(arr1);
+
+arr1.push("lihle");
+arr1.unshift("Ndindwa")
+console.log(arr1);
