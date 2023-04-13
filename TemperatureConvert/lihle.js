f847c00b7f7da20974d43eb39f8e3d9752c415e6
LNdindwa <lndindwa@gmail.com>
2023-04-13 12:01:20 +0200

    TEMPERATURECONVERT

--- TemperatureConvert/lihle.js
+++ TemperatureConvert/lihle.js
@@ -7,20 +7,28 @@
 // console.log(convertTemp(8))
 
 
-function convertTemp(fahrenheits){
+function convertTemp(){
     // getting user input
-    let f = document.getElementById("number").value 
-
+    let f = document.getElementById("convertTemp").value 
+    let feedback =document.getElementById("feedback")
     // converting temperature
-    let c = (f -32) * 5/9
+    let c = (f -32) * (5/9);
+    feedback.innerHTML = c
 
-    if(c < 16){
-        console.log('Cold', c)
-    }else if(c >= 16 && c < 21){
-        console.log('warm', c)
-    }else{
-        console.log('hot', c)
+    if(!f){
+    feedback.innerHTML =("please enter value")    
     }
+    else if(c < 16){
+        // console.log('Cold', c)
+        feedback.innerHTML =('Cold', c)
+    }else if(c >= 16 && c < 21){
+        // console.log('warm', c)
+        feedback.innerHTML =('warm', c)
+    }else{
+        // console.log('hot', c)
+        feedback.innerHTML =('hot', c)
+    }
+    
     // let celsius = (fahrenheits-32)*5/9;
     // if(celsius < 16){
     //     alert("its cold");
@@ -34,11 +42,11 @@ function convertTemp(fahrenheits){
 }
 // (convertTemp(27))
 
-let numbers = [5,4,3,2,1,0,1,2,3,4,5]
-let sum = 0;
+// let numbers = [5,4,3,2,1,0,1,2,3,4,5]
+// let sum = 0;
 
-for(let i = 0; i < numbers.length; i++ ){
-    sum += numbers[i];
+// for(let i = 0; i < numbers.length; i++ ){
+//     sum += numbers[i];
 
-}
-console.log(sum)
+// }
+// console.log(sum)
