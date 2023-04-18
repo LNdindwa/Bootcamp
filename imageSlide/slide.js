9845485e56636670828937011ad5b76981c16bb1
LNdindwa <lndindwa@gmail.com>
2023-04-18 17:09:17 +0200

    IMAGESLIDE

--- imageSlide/slide.js
+++ imageSlide/slide.js
@@ -1,54 +1,103 @@
 
-let show_images = document.querySelector(".show_images");
-let foward = document.querySelector(".foward");
-let prev = document.querySelector(".prev");
+let slideshow = document.querySelector(".slideshow");
+let Foward = document.querySelector(".Foward");
+let Previous  = document.querySelector(".Previous");
 
+let images;
 
-let imgs = ["images1.png","images2.png","images3.png"]
+if(localStorage.getItem("images")){
+   images = JSON.parse(localStorage.getItem("images"));
+
+}else{
+    images = ["images1.png","images2.png","images3.png"];
+
+    
+}
+// document.getElementById("slideshow")
+// sessionStorage.setItem("slideshow",JSON.stringify(images))
 
 let index = 0;
 
+
+
+// function add(){
+
+//     let img = document.getElementById("img").value;
+//     images.push(img)
+//     // document.getElementById("img").value = '';
+    
+//     sessionStorage.setItem("slideshow",JSON.stringify(images));
+// }
+// // images.src = images[index]
+
 function next(){
-show_images.src = imgs[index]
+    slideshow.src = images[index]
+
     // let show = imgs[index];
     // console.log(show);
     index++;
-    if(index >= imgs.length){
+    if(index >= images.length){
         index=0;
     }
-
+    // images.src = images[index]
 }
 
 function back(){
+    slideshow.src = images[index]
     // let show = image[i]
 // index = index-1;
-show_images.src = imgs[index]
-index--
-
+index --;
 if(index < 0){
-    index = imgs.length-1;
+    index = images.length-1;
+    index=0;
 }
 
 }
 
 
 
-let img = document.getElementById("image")
+// let img = document.getElementById("image")
 //let img = document.getElementById("image").value
-let current = 0;
+// let current = 0;
+
+    // let m = sessionStorage.getItem(JSON.parse("images"));
+
+
+
+// if(sessionStorage.getItem("images")){
+// images = JSON.parse(sessionStorage.getItem("images"));    
+// }else{
+//     images = ["images1.png","images2.png","images3.png"]
+// }
+
+// let pictures = ["images1.png","images2.png","images3.png"]
+
+// localStorage.setItem("images",JSON.stringify(pictures));
+// //localStorage.getItem(pictures)
+
+
 
 function add(){
-    let img = document.getElementById("image").value 
-    Image.push("image")
+
+    let imgs = document.getElementById("imgs").value;
+    images.push(imgs);
+    // document.getElementById("img").value = '';
+    
+    localStorage.setItem("images",JSON.stringify(images));
+
+    if(image =="" || image == null){
+        return "put something"
+
+    }else if(image){
+        return "this image already exists"
+    }
+
+
 }
 
-let pictures = ["images1.png","images2.png","images3.png"]
 
-localStorage.setItem("images",JSON.stringify(pictures));
-//localStorage.getItem(pictures)
-
-
-function nyfunction(){
-    let x = localStorage.getItem("");
-    document.getElementById("").innerHTML = x;
+function removeImage(){
+    images.splice(i,1);
+    localStorage.setItem("images",JSON.stringify(images))
+    window.location.reload()
 }
